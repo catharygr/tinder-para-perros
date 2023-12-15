36e3ff11a4cdec69acf84c8287b03b6c4cbf6a9f
catharygr <103951447+catharygr@users.noreply.github.com>
2023-12-15 18:42:14 +0100

    Agregando un perro más

--- data.js
+++ data.js
@@ -1,52 +1,58 @@
 export const dogs = [
-    {
-        name: "Simba",
-        avatar: "images/dog-simba.jpg",
-        age: 21,
-        bio: "Alguna perrita dispuesta a salir conmigo?",
-        hasBeenSwiped: false,
-        hasBeenLiked: false
-    },
-    {
-        name: "Rex",
-        avatar: "images/dog-rex.jpg",
-        age: 25,
-        bio: "Art. Literature. Natural y libre. Yoga.",
-        hasBeenSwiped: false,
-        hasBeenLiked: false
-    },
-    {
-        name: "Aivar",
-        avatar: "images/dog-aivar.jpg",
-        age: 32,
-        bio: "Hola soy un perro canario. Alguna perrita que me quiera",
-        hasBeenSwiped: false,
-        hasBeenLiked: false
-    },
-    {
-        name: "Teddy",
-        avatar: "images/dog-teddy.jpg",
-        age: 30,
-        bio: "Os gusta mi mirada?",
-        hasBeenSwiped: false,
-        hasBeenLiked: false
-    },
-    {
-            name: "Zuhur",
-            avatar: "images/dog-zuhur.jpg",
-            age: 60,
-            bio: "Soy un perro vasco",
-            hasBeenSwiped: false,
-            hasBeenLiked: false
-        
-    },
-    {
-        name: "Duda",
-        avatar: "images/dog-duda.jpg",
-        age: 18,
-        bio: "Soy una perrita de agua, os animáis a conocerme?",
-        hasBeenSwiped: false,
-        hasBeenLiked: false
-    }
-]
-
+  {
+    name: "Simba",
+    avatar: "images/dog-simba.jpg",
+    age: 21,
+    bio: "Alguna perrita dispuesta a salir conmigo?",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+  {
+    name: "Kovu",
+    avatar: "images/dog-kovu.jpg",
+    age: 18,
+    bio: "Soy un perrito muy joven y travieso, os animáis a conocerme?",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+  {
+    name: "Rex",
+    avatar: "images/dog-rex.jpg",
+    age: 25,
+    bio: "Art. Literature. Natural y libre. Yoga.",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+  {
+    name: "Aivar",
+    avatar: "images/dog-aivar.jpg",
+    age: 32,
+    bio: "Hola soy un perro canario. Alguna perrita que me quiera",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+  {
+    name: "Teddy",
+    avatar: "images/dog-teddy.jpg",
+    age: 30,
+    bio: "Os gusta mi mirada?",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+  {
+    name: "Zuhur",
+    avatar: "images/dog-zuhur.jpg",
+    age: 60,
+    bio: "Soy un perro vasco",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+  {
+    name: "Duda",
+    avatar: "images/dog-duda.jpg",
+    age: 18,
+    bio: "Soy una perrita de agua, os animáis a conocerme?",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+];
